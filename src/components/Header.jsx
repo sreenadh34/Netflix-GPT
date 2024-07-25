2a2797573dc5e859b9c06df8416874f086d9f838
sreenadh34 <43.sreenadh@gmail.com>
2024-07-25 17:57:16 +0530

    Multi Language Support in GPT Search page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser} from "../utils/userSlice";
+import { toggleGptSearchView } from '../utils/gptSlice';
 const Header = () => {
 
 const navigate =useNavigate();
@@ -33,12 +34,22 @@ const user =useSelector(store => store.user);
       });
       return () => unsubscribe();
     },[])
+
+    const handleGptSearchClick = () => {
+      dispatch(toggleGptSearchView());
+    }
   return (
      <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
         <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
         
         {user && (
-         <div className='flex p-2'>       
+         <div className='flex p-2'> 
+         <button 
+          className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg'
+          onClick={handleGptSearchClick}
+          >
+          GPT Search 
+          </button>      
          <img className='w-12 h-12' src= {user?.photoURL} alt="user Icon" />
        <button onClick={handleSignOut} className='font-bold text-white'>(Sign Out)</button>
     </div>
--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesSlice from "./moviesSlice";
+import gptReducer from "./gptSlice";
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     movies: moviesSlice,
+    gpt: gptReducer,
     
   },
 });
